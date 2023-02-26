df0bed208ff132d50677a4610cc39d10b343cd37
MattCMcCoy <mccoy.mat@northeastern.edu>
2023-02-26 14:39:49 -0500

    remove drone video locally add uploaded video

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,16 +1,8 @@
 /// <reference path='../index.d.ts' />;
-import React from 'react';
 import { TypeAnimation } from 'react-type-animation';
-import drone from '../images/Drone-Video.mp4';
-import droneImg from '../images/videoSub.jpeg';
+import ReactPlayer from 'react-player';
 
 export default function Home() {
-  const [isVideoLoaded, setIsVideoLoaded] = React.useState(false);
-
-  const onLoadedData = () => {
-    setIsVideoLoaded(true);
-  };
-
   const welcomeMessage = [
     'Hey, My Name Is Matt! :)\n I Enjoy Photography.',
     2000,
@@ -23,24 +15,14 @@ export default function Home() {
   ];
   return (
     <div className='flex min-h-screen justify-center items-center'>
-      <img
-        src={droneImg}
-        alt='DroneImage'
-        style={{ opacity: isVideoLoaded ? 0 : 1 }}
-      />
-      <video
-        className='object-cover w-[100vw] h-[100vh] absolute top-0 left-0 z-10'
-        id='dronevideo'
-        src={drone}
-        muted
-        autoPlay
-        playsInline
-        loop
-        onLoadedData={onLoadedData}
-        style={{ opacity: isVideoLoaded ? 1 : 0 }}
-      >
-        drone video
-      </video>
+      <div className='video-background'>
+        <iframe
+          title='droneVideo'
+          src='https://www.youtube.com/embed/mHtdM5ebXaE?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&mute=1&playlist=mHtdM5ebXaE'
+          frameBorder='0'
+          allowFullScreen
+        ></iframe>
+      </div>
       <div className='m-auto z-20 block absolute'>
         <TypeAnimation
           sequence={welcomeMessage}
