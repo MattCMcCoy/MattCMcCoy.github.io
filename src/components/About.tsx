1555ba8dedd78c83e2f0a3910237148e7d76d0e2
MattCMcCoy <mccoy.mat@northeastern.edu>
2023-02-27 12:07:55 -0500

    Cleaned up about me

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,34 +13,51 @@ export default function About() {
   ];
 
   return (
-    <div className='md:ml-10 m-5'>
-      <div className='lg:grid-cols-[400px_auto] grid-cols-1 flex-grid lg:grid'>
-        <div className='bg-slate-400 w-[60vw] max-w-[400px] max-h-[500px] h-[50vh] md:mt-[15vh] mt-[10vh] justify-center flex'>
-          <img
-            src={BigRockHike}
-            height={'100%'}
-            width={'100%'}
-            alt='hiking'
-            className='p-3'
-          />
-        </div>
-        <div
-          id='AboutMe'
-          className={clsx(
-            'mt-[10vh] mb-10 space-y-7 text-3xl font-bold font-sans md:p-5',
-            checkedState ? 'text-white' : 'text-slate-700'
-          )}
-        >
-          About Me
-          <div className='text-xl font-serif'>
-            {aboutMeDescription.map((line) => (
-              <>
-                <br />
-                <span>{line}</span>
-              </>
-            ))}
+    <div className='h-[100%]'>
+      <div className='md:ml-10 m-5'>
+        <div className='lg:grid-cols-[400px_auto] grid-cols-1 lg:grid'>
+          <div className='bg-slate-400 w-[60vw] max-w-[400px] max-h-[500px] h-[50vh] md:mt-[15vh] mt-[10vh] justify-center flex'>
+            <img
+              src={BigRockHike}
+              height={'100%'}
+              width={'100%'}
+              alt='hiking'
+              className='p-3'
+            />
+          </div>
+          <div
+            id='AboutMe'
+            className={clsx(
+              'mt-[10vh] space-y-7 text-3xl font-bold font-sans md:p-5 mb-10',
+              checkedState ? 'text-white' : 'text-slate-700'
+            )}
+          >
+            About Me
+            <div className='text-xl font-serif'>
+              {aboutMeDescription.map((line) => (
+                <>
+                  <br />
+                  <span>{line}</span>
+                </>
+              ))}
+            </div>
           </div>
         </div>
+      </div>
+      <div
+        className={clsx(
+          'p-2 bottom-0 relative lg:absolute h-30 w-[100vw] text-xl font-bold font-serif',
+          checkedState ? 'bg-slate-700' : 'bg-slate-500'
+        )}
+      >
+        <span>Contact Me</span>
+        <br />
+        <br />
+        Email: mccoy.mat@northeastern.edu
+        <br />
+        Phone: {'(617) 945-3436'}
+        <br />
+        <br />
       </div>
     </div>
   );
