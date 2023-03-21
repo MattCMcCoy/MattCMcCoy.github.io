4ee50ac4dc6f3c60c960e5da673c8cf691989470
MattCMcCoy <mccoy.mat@northeastern.edu>
2023-03-21 16:56:06 -0400

    download button in resume, and update resume

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,11 +1,22 @@
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
+import { Button } from 'reactstrap';
 
 export default function Resume() {
   return (
-    <div className='container'>
-      <Document file='/resume.pdf'>
-        <Page pageNumber={1}></Page>
-      </Document>
+    <div className='space-y-5'>
+      <div className='container space-y-5'>
+        <Document file='/resume.pdf'>
+          <Page pageNumber={1}></Page>
+        </Document>
+        <Button
+          href={'/resume.pdf'}
+          target={'_blank'}
+          className='m-auto w-40 mt-1 block'
+          download
+        >
+          Download
+        </Button>
+      </div>
     </div>
   );
 }
